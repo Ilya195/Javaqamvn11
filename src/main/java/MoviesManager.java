be50a11759806f6fa7f3d7120d2783803818dfec
Your Name <you@example.com>
2022-06-28 14:08:32 +0600

    Initial commit

---
+++ src/main/java/MoviesManager.java
@@ -0,0 +1,39 @@
+public class MoviesManager {
+    protected String[] movies = new String[0];
+    protected int limit;
+
+    public MoviesManager(int limit) {
+        this.limit = limit;
+    }
+
+    public MoviesManager() {
+        limit = 10;
+    }
+
+    public void add(String film) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = film;
+        movies = tmp;
+    }
+
+    public String[] findAll() {
+        return movies;
+    }
+
+    public String[] findLast() {
+        int result = 0;
+        if (movies.length < limit) {
+            result = movies.length;
+        } else {
+            result = limit;
+        }
+        String[] track = new String[result];
+        for (int i = 0; i < track.length; i++) {
+            track[i] = movies[movies.length - 1 - i];
+        }
+        return track;
+    }
+}
---
+++ src/test/java/MoviesManagerTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoviesManagerTest {
+
+    @Test
+    public void managerFilmTest() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.add("F1");
+
+        String[] actual = manager.findAll();
+        String[] expected = {"F1"};
+
+        Assertions.assertArrayEquals(actual, expected);
+
+    }
+
+    @Test
+    public void managerFilmsTest() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.add("F1");
+        manager.add("F2");
+        manager.add("F3");
+        manager.add("F4");
+
+        String[] actual = manager.findAll();
+        String[] expected = {"F1", "F2", "F3", "F4"};
+
+        Assertions.assertArrayEquals(actual, expected);
+
+    }
+
+    @Test
+    public void managerMaxFilmsTest() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.add("F1");
+        manager.add("F2");
+        manager.add("F3");
+        manager.add("F4");
+        manager.add("F5");
+        manager.add("F6");
+        manager.add("F7");
+        manager.add("F8");
+        manager.add("F9");
+        manager.add("F10");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"F10","F9", "F8", "F7", "F6", "F5", "F4", "F3", "F2", "F1"};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+}
